ddb388a8b9c5281a74a0f88e348939de855bb940
Juni0505 <dudwns050505@gmail.com>
2024-01-02 20:59:35 +0900

    BaekJoon Solve
    Solve

---
+++ src/level1/Cat.java
@@ -0,0 +1,14 @@
+package level1;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+
+}
---
+++ src/level2/TwoNumberCompare.java
@@ -0,0 +1,21 @@
+package level2;
+
+import java.util.Scanner;
+
+public class TwoNumberCompare {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		if(n1 > n2) {
+			System.out.println(">");
+		}else if(n1 < n2) {
+			System.out.println("<");
+		}else {
+			System.out.println("==");
+		}
+		sc.close();
+	}
+
+}
