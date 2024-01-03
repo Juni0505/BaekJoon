71f2df65730d65112a1f7a8c41e1c292dc8dbd7d
Juni0505 <dudwns050505@gmail.com>
2024-01-03 23:50:33 +0900

    BaekJoon Solve
    단계별로 하루 1커밋 목표

---
+++ src/level3/RepeatedNumber.java
@@ -0,0 +1,18 @@
+package level3;
+
+import java.util.Scanner;
+
+public class RepeatedNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		for(int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
