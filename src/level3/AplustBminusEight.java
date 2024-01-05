08ef4f7a42032dcdceb094e14d1a266211115fc9
Juni0505 <dudwns050505@gmail.com>
2024-01-05 18:27:58 +0900

    BaekJoon level3
    Solve

---
+++ src/level3/AplustBminusEight.java
@@ -0,0 +1,20 @@
+package level3;
+
+import java.util.Scanner;
+
+public class AplustBminusEight {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 1; i <= n; i++) {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			arr[i-1] = n1+n2;
+			System.out.println("Case #"+i+": "+ n1+" + " +n2+" = "+arr[i-1]);
+		}
+		sc.close();
+	}
+
+}
