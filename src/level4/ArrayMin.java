9cafbe77d96a7e0db48a13022540aa77beda9f1b
Juni0505 <dudwns050505@gmail.com>
2024-01-09 23:46:26 +0900

    BaekJoon level4 Solve
    Solve and 101811 TODO

--- src/level4/ArrayMin.java
+++ src/level4/ArrayMin.java
@@ -6,10 +6,16 @@ public class ArrayMin {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for(int i = 0; i < 29; i++) {
-			int a = sc.nextInt();
-			
+		int[] arr = new int[31];
+		for (int i = 1; i < 29; i++) {
+			int n = sc.nextInt();
+			arr[n] = 1;
 		}
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] != 1)
+				System.out.println(i);
+		}
+
 		sc.close();
 	}
 
