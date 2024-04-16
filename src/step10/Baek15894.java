c8eb40d2e3b6408cffa6d538da68811a395cbf3d
Juni0505 <dudwns050505@gmail.com>
2024-04-16 20:41:49 +0900

    BaekJoon
    집안일 문제로 잠시 스탑 후 재개

---
+++ src/step10/Baek15894.java
@@ -0,0 +1,14 @@
+package step10;
+
+import java.util.Scanner;
+
+public class Baek15894 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        sc.close();
+
+        long b = a * 4;
+        System.out.println(b);
+    }
+}
