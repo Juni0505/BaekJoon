f88c62678be32bf548591d6e8d9cde25f90d6777
Juni0505 <dudwns050505@gmail.com>
2024-04-20 23:25:10 +0900

    BaekJoon
    알고리즘 문제

---
+++ src/step12/Baek19532.java
@@ -0,0 +1,23 @@
+package step12;
+
+import java.util.Scanner;
+
+public class Baek19532 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int c = sc.nextInt();
+	        int d = sc.nextInt();
+	        int e = sc.nextInt();
+	        int f = sc.nextInt();
+
+	        int x = (c * e - b * f) / (a * e - b * d);
+	        int y = (a * f - c * d) / (a * e - b * d);
+
+	        System.out.println(x + " " + y);
+
+	        sc.close();
+	    }
+}
